aabf0f3594633eae3c9478af46449d42478195f9
Noman5237 <anonyman637@gmail.com>
2023-02-09 12:35:08 +0600

    code changed

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. </h1>";
+return "<h1> Congratulations. Continuous Deployment Active </h1>";
 }
 }
